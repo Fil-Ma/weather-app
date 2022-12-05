b56c78b0c0b7c0918c5265acef9bd59161d05407
Filippo <filippo.mazzalai@gmail.com>
2022-12-05 16:49:06 +0100

    added header and footer to app

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,31 @@
 import React from "react";
+import Button from "@mui/material/Button";
 
 export default function Header() {
+    
+    function handleClickLogin() {
+
+    }
+
+    function handleClickRegister() {
+
+    }
 
     return (
         <header>
             <div className="logo">
                 Logo
             </div>
+
+            <Button 
+                variant="outlined"
+                onClick={handleClickLogin}
+            >Login</Button>
+
+            <Button 
+                variant="contained"
+                onClick={handleClickRegister}
+            >Register</Button>
         </header>
     )
 }
