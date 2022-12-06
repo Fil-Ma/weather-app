b1ffa9c05fe679b870dadbb5f924ea9a17463f51
Filippo <filippo.mazzalai@gmail.com>
2022-12-06 11:36:12 +0100

    moved footer and header, added css files, added basic example to test the api

--- src/app/App.js
+++ src/app/App.js
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
+
+import { retrieveWeatherData } from "../api";
 
 export default function App() {
+    const [weatherData, setWeatherData] = useState(null);
+    const weatherWidget = document.getElementById("weather-widget");
+
+    // handle click on button main
+    async function handleClick(event) {
+        event.preventDefault();
+        try {
+            const data = await retrieveWeatherData({
+                latitude: 46.04,
+                longitude: 11.07
+            });
+            setWeatherData(data);
+            console.log("wheater data", data)
+        } catch(err) {
+            console.log(err);
+        }
+    }
 
     return (
         <>
             <Header />
             <main>
                 <h1>This is a title</h1>
+                <button onClick={handleClick}>Click here to retrieve data</button>
+                <div id="weather-widget">
+
+                </div>
             </main>
             <Footer />
         </>
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,3 +1,4 @@
+import "./footer.css";
 import React from "react";
 
 export default function Footer() {
