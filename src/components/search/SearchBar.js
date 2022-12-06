0206dc97e9064d7ae589cfef9baa348d58432878
Filippo <filippo.mazzalai@gmail.com>
2022-12-06 17:05:59 +0100

    added search component, added mui example

---
+++ src/components/search/SearchBar.js
@@ -0,0 +1,13 @@
+import "./search-bar.css";
+import React from "react";
+
+export default function SearchBar({ handleSubmit }) {
+
+    return (
+        <form className="search-bar" onSubmit={handleSubmit}>
+            {/* search bar with google location autocomplete */}
+            
+        </form>
+    )
+}
+
