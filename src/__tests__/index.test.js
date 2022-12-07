06f08e55ea24badf75583fd51395d8c8ce6dd38f
Filippo <filippo.mazzalai@gmail.com>
2022-12-07 17:58:03 +0100

    fixed app test

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { render } from "@testing-library/react";
 
+import ThemeContextProvider from "../contexts/ThemeContext";
+import LanguageContextProvider from "../contexts/LanguageContext";
 import App from "../app/App";
 
 describe("App function component", () => {
 
     it("renders without errors", () => {
-        render(<App />);
+        render(
+            <ThemeContextProvider>
+                <LanguageContextProvider>
+                    <App />
+                </LanguageContextProvider>
+            </ThemeContextProvider>
+        );
     })
 })
