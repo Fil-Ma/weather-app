6516c5a0f8f08866d05b3522aa61c4535ceab223
Filippo <filippo.mazzalai@gmail.com>
2022-12-12 12:56:21 +0100

    changed button text, removed align self

--- src/components/PositionSearch.js
+++ src/components/PositionSearch.js
@@ -5,15 +5,19 @@ import {
 } from "@mui/material";
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 
-export default function PositionSearch({ handleGeopositionSearch }) {
+export default function PositionSearch({ hasGeolocation, handleGeopositionSearch }) {
 
     return (
-        <Box sx={{ width: "23%", alignSelf: "flex-end" }}>
-            <Button
-                sx={{ backgroundColor: "primary.main", color: "primary.contrastText" }}
-                onClick={handleGeopositionSearch}>
-                    <LocationOnIcon /> Or use your current position
-            </Button>
+        <Box sx={{ width: "23%" }}>
+            {
+                hasGeolocation && (
+                    <Button
+                        sx={{ backgroundColor: "primary.main", color: "primary.contrastText" }}
+                        onClick={handleGeopositionSearch}>
+                            <LocationOnIcon /> use position
+                    </Button>
+                )
+            }
         </Box>
     )
 }
