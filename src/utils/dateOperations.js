9b45b5e4df4809dcf0ff64615b4d7f7f44748774
Filippo <filippo.mazzalai@gmail.com>
2022-12-13 19:08:35 +0100

    added data normalization function, improved date conversion

--- src/utils/dateOperations.js
+++ src/utils/dateOperations.js
@@ -2,22 +2,37 @@
  * Converts epoch format data to local.
  * 
  * @param { Object } epochDate | date in epoch format
- * @returns { Object } local date, local format
+ * @returns { Object } local date, local time
  */
-export function dateToString(epochDate) {
+export function dateToString(epochDate, language) {
+    // transfom date to normal format from epoch seconds
     let currentDateTime = new Date(0);
     const dateEpoch = epochDate
     currentDateTime.setUTCSeconds(dateEpoch);
 
-    const currentDate = currentDateTime.toLocaleDateString(
-        undefined, 
-        {
-            weekday: "long", 
-            year: "numeric", 
-            month: "long", 
-            day: "numeric"
-        }
-    );
-    const currentTime = currentDateTime.toLocaleTimeString();
-    return { currentDate, currentTime }
+    // Transform language to Language Tags (BCP 47)
+    let standardLanguageTag;
+    if (language === "en") {
+        standardLanguageTag = "en-US"
+    } else if (language === "it") {
+        standardLanguageTag = "it-IT"
+    }
+
+    // date representation options
+    const dateOptions = {
+        weekday: "short",
+        month: "long", 
+        day: "numeric"
+    }
+
+    // time representation options
+    const timeOptions = {
+        hour: "numeric",
+        minute: "numeric"
+    }
+
+    const date = currentDateTime.toLocaleDateString(standardLanguageTag, dateOptions);
+    const time = currentDateTime.toLocaleTimeString(standardLanguageTag, timeOptions);
+
+    return { date, time }
 }
