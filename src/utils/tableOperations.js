79868e491491353d6bd96342a14e95154c94b2ee
Filippo <filippo.mazzalai@gmail.com>
2022-12-15 13:13:54 +0100

    added utils for hourly data

--- src/utils/tableOperations.js
+++ src/utils/tableOperations.js
@@ -1,4 +1,5 @@
 import { normalizeData } from "./dataNormalization";
+import { normalizeHourlyData } from "./dataNormalization";
 /**
  * Creates rows data from weather daily data object.
  * 
@@ -59,6 +60,60 @@ export function createTableData(dailyDataObject, language) {
         rows.moonrise.push(normalizedObject.moonrise)
         rows.moonset.push(normalizedObject.moonset)
         rows.moonphase.push(normalizedObject.moonphase)
+    })
+
+    return { columnLabels, rows }
+}
+
+/**
+ * Creates rows data from weather hourly data object.
+ * 
+ * @param {Object} hourlyData
+ * @returns {Object} object of arrays
+ */
+export function createHourlyTableData(hourlyDataObject, language) {
+    const columnLabels = [];
+    const rows = {
+        image: [],
+        description: [],
+        precipitationProbability: [],
+        rain: [],
+        snow: [],
+        temperatureCelsius: [], // array min max
+        feelsLikeCelsius: [], // array day night
+        temperatureFahrenheit: [], // array
+        feelsLikeFahrenheit: [], // array
+        // pressure: [],
+        humidity: [],
+        // windSpeed: [],
+        //windDirection: []
+    }
+
+    hourlyDataObject.forEach((hour) => {
+        const normalizedObject = normalizeHourlyData(hour, language);
+
+        // column labels
+        columnLabels.push(normalizedObject.time)
+
+        // status column
+        rows.image.push(normalizedObject.statusImage)
+        rows.description.push(normalizedObject.statusDescription)
+
+        // precipitation, rain, snow volumes
+        rows.precipitationProbability.push(normalizedObject.precipitationProbability)
+        rows.rain.push(normalizedObject?.rain ? normalizedObject.rain : "-")
+        rows.snow.push(normalizedObject?.snow ? normalizedObject.snow : "-")
+
+        // temperatures
+        rows.temperatureCelsius.push(normalizedObject.temperatureCelsius)
+        rows.temperatureFahrenheit.push(normalizedObject.temperatureFahrenheit)
+        rows.feelsLikeCelsius.push(normalizedObject.feelsLikeCelsius)
+        rows.feelsLikeFahrenheit.push(normalizedObject.feelsLikeFahrenheit)
+        
+       // rows.pressure.push(normalizedObject.pressure)
+        rows.humidity.push(normalizedObject.humidity)
+        // rows.windSpeed.push(normalizedObject.windSpeed)
+        // rows.windDirection.push(normalizedObject.windDirection)
     })
 
     return { columnLabels, rows }
