c5b8cec07557778560620fe9f2d083a115a8486b
Filippo <filippo.mazzalai@gmail.com>
2022-12-15 10:39:02 +0100

    introduced createTableData function

--- src/components/DailyForecastTable.js
+++ src/components/DailyForecastTable.js
@@ -11,65 +11,11 @@ import {
 } from "@mui/material";
 
 import { useLanguageContext } from "../contexts/LanguageContext";
-import { normalizeData } from "../utils/dataNormalization";
+import { createTableData } from "../utils/tableOperations";
 
 export default function DailyForecastTable({ dailyData }) {
     const { language, dictionary } = useLanguageContext();
-
-    const columnLabels = [];
-    const rows = {
-        image: [],
-        description: [],
-        precipitationProbability: [],
-        rain: [],
-        snow: [],
-        temperatureCelsius: [], // array min max
-        feelsLikeCelsius: [], // array day night
-        temperatureFahrenheit: [], // array
-        feelsLikeFahrenheit: [], // array
-        pressure: [],
-        humidity: [],
-        windSpeed: [],
-        windDirection: [],
-        sunrise: [],
-        sunset: [],
-        moonrise: [],
-        moonset: [],
-        moonphase: []
-    }
-
-    dailyData.forEach((day) => {
-        const normalizedObject = normalizeData(day, language);
-
-        // column labels
-        columnLabels.push(normalizedObject.date)
-
-        // status column
-        rows.image.push(normalizedObject.statusImage)
-        rows.description.push(normalizedObject.statusDescription)
-
-        // precipitation, rain, snow volumes
-        rows.precipitationProbability.push(normalizedObject.precipitationProbability)
-        rows.rain.push(normalizedObject?.rain ? normalizedObject.rain : "-")
-        rows.snow.push(normalizedObject?.snow ? normalizedObject.snow : "-")
-
-        // temperatures
-        rows.temperatureCelsius.push(normalizedObject.temperatureCelsius)
-        rows.temperatureFahrenheit.push(normalizedObject.temperatureFahrenheit)
-        rows.feelsLikeCelsius.push(normalizedObject.feelsLikeCelsius)
-        rows.feelsLikeFahrenheit.push(normalizedObject.feelsLikeFahrenheit)
-        
-        rows.pressure.push(normalizedObject.pressure)
-        rows.humidity.push(normalizedObject.humidity)
-        rows.windSpeed.push(normalizedObject.windSpeed)
-        rows.windDirection.push(normalizedObject.windDirection)
-
-        rows.sunrise.push(normalizedObject.sunrise)
-        rows.sunset.push(normalizedObject.sunset)
-        rows.moonrise.push(normalizedObject.moonrise)
-        rows.moonset.push(normalizedObject.moonset)
-        rows.moonphase.push(normalizedObject.moonphase)
-    })
+    const { columnLabels, rows } = createTableData(dailyData, language)
 
     return (
         <Box 
