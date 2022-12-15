9ac65ce96f045da0edb5dd58305112599cb14179
Filippo <filippo.mazzalai@gmail.com>
2022-12-15 13:14:35 +0100

    displayed hourly data with table

--- src/components/ForecastContainer.js
+++ src/components/ForecastContainer.js
@@ -8,7 +8,7 @@ import NoData from "./NoData";
 export default function ForecastContainer({ weatherData }) {
 
     return (
-        <Box component="section">
+        <Box component="section" sx={{ position: "relative" }}>
             {/* message to display if no location has been entered */}
             {
                 !weatherData && <NoData />
@@ -24,29 +24,13 @@ export default function ForecastContainer({ weatherData }) {
             {/* daily forecast */}
             {
                 weatherData?.daily && 
-                <DailyForecastTable dailyData={weatherData?.daily.slice(0, 6)} />
+                <DailyForecastTable dailyData={weatherData.daily.slice(0, 6)} />
             }
-        
-            {/* hourly forecast */}
-            <Box sx={{ mb: "4rem" }}>
-                <Box 
-                    sx={{ 
-                        display: "flex", 
-                        flexDirection: "row", 
-                        flexWrap: "wrap", 
-                        justifyContent: "space-around", 
-                        mt: "2rem" 
-                    }}>
-                        {   
-                            weatherData?.hourly &&
-                            weatherData.hourly.slice(0, 24).map((hourData, index) => {
-                                return <HourlyForecast 
-                                            key={index} 
-                                            hourForecast={hourData} />
-                            })
-                        }
-                </Box>
-            </Box>
+
+            {
+                weatherData?.hourly &&
+                <HourlyForecast hourlyData={weatherData.hourly.slice(0, 24)} />
+            }
         </Box>
     )
 }
--- src/components/HourlyForecast.js
+++ src/components/HourlyForecast.js
@@ -1,34 +1,98 @@
 import React from "react";
 import {
     Box, 
-    Typography
+    Typography,
+    TableContainer,
+    Table,
+    TableHead,
+    TableBody,
+    TableRow,
+    TableCell
 } from "@mui/material";
+import { useLanguageContext } from "../contexts/LanguageContext";
+import { createHourlyTableData } from "../utils/tableOperations";
 
-export default function HourlyForecast({ hourForecast }) {
-    const hour = hourForecast.dt;
-    const feelsLikeTemperature = hourForecast.feels_like;
-    const temperature = hourForecast.temp;
-    const pressure = hourForecast.pressure;
-    const humidity = hourForecast.humidity;
-
-    const statusMain = hourForecast.weather[0].main;
-    const statusDescription = hourForecast.weather[0].main;
-    const windSpeed = hourForecast.wind_speed;
-    const windDirection = hourForecast.wind_deg;
+export default function HourlyForecast({ hourlyData }) {
+    const { language, dictionary } = useLanguageContext()
+    const { columnLabels, rows } = createHourlyTableData(hourlyData, language);
 
     return (
-        <Box sx={{ height: "5rem", overflow: "scroll" }}>
-            <Typography>{hour}</Typography>
-            <Typography>{statusMain}</Typography>
-            <Typography>{statusDescription}</Typography>
+        <Box 
+            sx={{ 
+                minHeight: "30rem",
+                backgroundColor: "background.paper",
+                color: "primary.contrastText",
+                border: "1px solid rgba(0,0,0,0.3)",
+                borderRadius: "1rem",
+                p: "1rem 2rem",
+            }}>
+                <Typography component="h2">{dictionary.forecast.hourly.title}</Typography>
+                <TableContainer sx={{ mt: "2rem" }}>
+                    <Table stickyHeader aria-label="sticky table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell sx={{ backgroundColor: "primary.main", color: "primary.contrastText" }}>
 
-            <Typography>{humidity}</Typography>
-            <Typography>{pressure}</Typography>
-            <Typography>{temperature}</Typography>
-            <Typography>{feelsLikeTemperature}</Typography>
-            
-            <Typography>{windSpeed}</Typography>
-            <Typography>{windDirection}</Typography>
+                                </TableCell>
+                                {
+                                    columnLabels.map((hourTime, index) => {
+                                        return (
+                                            <TableCell 
+                                                align="center" 
+                                                key={index} 
+                                                sx={{ 
+                                                    backgroundColor: "primary.main", 
+                                                    color: "#FFF"
+                                                }}>
+                                                    {
+                                                        hourTime
+                                                    }
+                                            </TableCell>
+                                        )
+                                    })
+                                }
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {
+                                Object.keys(rows).map((rowKey, index) => {
+                                    return (
+                                        <TableRow key={index}>
+                                            <TableCell
+                                                sx={{ 
+                                                    backgroundColor: "background.paper", 
+                                                    color: "primary.contrastText",
+                                                    position: "sticky",
+                                                    left: 0
+                                                }}
+                                                align="left">
+                                                    {
+                                                        dictionary.forecast.hourly["row-labels"][rowKey]
+                                                    }
+                                            </TableCell>
+                                            {
+                                                rows[rowKey].map((element, index) => {
+                                                    return (
+                                                        <TableCell 
+                                                            align="center" 
+                                                            sx={{ 
+                                                                backgroundColor: "background.paper", 
+                                                                color: "primary.contrastText",
+                                                                minWidth: "5rem" 
+                                                            }}
+                                                            key={index}>
+                                                                {element}
+                                                        </TableCell>
+                                                    )
+                                                })
+                                            }
+                                        </TableRow>
+                                    )
+                                })
+                            }
+                        </TableBody>
+                    </Table>
+                </TableContainer>
         </Box>
     )
 }
